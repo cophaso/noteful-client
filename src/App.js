e0ab818cd261d20d7c9c541136063b49d8ce102f
Crystal Ophaso <crystalo@procore.com>
2019-08-12 12:23:58 -0700

    add api request and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
-import dummyStore from './dummy-store';
 import NotePage from './NotePage/NotePage';
 import NoteList from './NoteList/NoteList';
 import FolderListNav from './FolderListNav/FolderListNav';
@@ -14,17 +13,42 @@ class App extends Component {
   };
 
   componentDidMount() {
-      // fake date loading from API call
-      setTimeout(() => this.setState(dummyStore), 600);
+    Promise.all([
+      fetch(`http://localhost:9090/notes`),
+      fetch(`http://localhost:9090/folders`)
+    ])
+    .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok)
+            throw new Error(notesRes.status)
+        if (!foldersRes.ok)
+          throw new Error(foldersRes.status)
+
+        return Promise.all([notesRes.json(), foldersRes.json()]);
+    })
+    .then(([notes, folders]) => {
+        this.setState({notes, folders});
+    })
+    .catch(error => {
+        console.error({error});
+    });
   }
+
+
+  handleDeleteNote = noteId => {
+    this.setState({
+        notes: this.state.notes.filter(note => note.id !== noteId)
+    });
+  };
 
   render(){
     const contextValue = {
       notes: this.state.notes,
-      folders: this.state.folders
+      folders: this.state.folders,
+      deleteNote: this.handleDeleteNote
     }
     return (
       <div className="App">
+        {console.log(contextValue)}
         <NotefulContext.Provider value={contextValue}>
           <nav className="App__nav">
           {['/', '/folder/:folderId'].map(path => (
--- src/NoteItem/NoteItem.js
+++ src/NoteItem/NoteItem.js
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import './NoteItem.css';
+import NotefulContext from '../NotefulContext';
 
 
-function NoteItem(props) {
-  return(
-    <li className='Note__Item' key={props.id}>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`} >
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
-          {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
+class NoteItem extends Component {
+  static defaultProps ={
+    onDeleteNote: () => {},
+  }
+  static contextType = NotefulContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteId = this.props.id
+
+    fetch(`http://localhost:9090/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(() => {
+        this.context.deleteNote(noteId)
+        // allow parent to perform extra behaviour
+        this.props.onDeleteNote(noteId)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+  render(){
+    const { name, id, modified } = this.props
+    return(
+      <li className='Note__Item' key={id}>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`} >
+            {name}
+          </Link>
+        </h2>
+        <button 
+          className='Note__delete' 
+          type='button'
+          onClick={this.handleClickDelete}
+        >
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {format(modified, 'Do MMM YYYY')}
+            </span>
+          </div>
         </div>
-      </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 export default NoteItem
